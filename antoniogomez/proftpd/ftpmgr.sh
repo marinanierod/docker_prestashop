cf0f2b61ea601de0856cc4bc3fb3667256436b20
usuario3 <antoniomgomezvarela@gmail.com>
2017-02-08 21:22:37 +0100

    Eliminada linea

--- antoniogomez/proftpd/ftpmgr.sh
+++ antoniogomez/proftpd/ftpmgr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/sbin/proftpd -d
+/usr/sbin/proftpd
 CHOICE='foo'
 while [[ ${CHOICE,,} != 'salir' ]]; do
         printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
