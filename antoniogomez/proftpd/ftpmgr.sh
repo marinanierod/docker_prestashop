0ac8b2f96ec9b3cf35a97393fbca3853051ab2a7
usuario3 <antoniomgomezvarela@gmail.com>
2017-02-08 21:09:47 +0100

    Cambiado de bin a sbin

--- antoniogomez/proftpd/ftpmgr.sh
+++ antoniogomez/proftpd/ftpmgr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/proftpd -d -c /etc/proftpd/proftpd.conf
+/usr/sbin/proftpd -d -c /etc/proftpd/proftpd.conf
 CHOICE='foo'
 while [[ ${CHOICE,,} != 'salir' ]]; do
         printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
