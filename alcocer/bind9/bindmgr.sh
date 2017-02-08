4ba81350b15a04b44d5f9c60593ee816cbde5076
marinanierod <marnierod@gmail.com>
2017-02-08 20:06:22 +0100

    branch

--- alcocer/bind9/bindmgr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/sbin/named -u bind
-
-CHOICE='foo'
-while [[ ${CHOICE,,} != 'salir' ]]; do
-        printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
-        read -p 'Bind9 hosting cmd: ' CHOICE
-done
