c6f84d04f943e8f32cb36907fdce1fa34f709e69
usuario3 <antoniomgomezvarela@gmail.com>
2017-02-08 20:47:26 +0100

    Añadido ftpmgr

---
+++ antoniogomez/proftpd/ftpmgr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/bin/proftpd -d -c /etc/proftpd/proftpd.conf
+CHOICE='foo'
+while [[ ${CHOICE,,} != 'salir' ]]; do
+        printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
+        read -p 'Proftpd hosting cmd: ' CHOICE
+done
