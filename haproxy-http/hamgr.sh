f9539c6f082303288c470ca07e55ddb9a79eb1d9
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-02-12 11:05:34 +0100

    compose de pruebas

---
+++ haproxy-http/hamgr.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+/usr/sbin/haproxy -f /etc/haproxy/haproxy.conf -Ds
+
+CHOICE='foo'
+while [[ ${CHOICE,,} != 'salir' ]]; do
+        printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
+        read -p 'haproxy hosting cmd: ' CHOICE
+done
+/usr/bin/killall haproxy
