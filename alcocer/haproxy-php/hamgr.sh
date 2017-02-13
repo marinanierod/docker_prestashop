8e75f072decf46635f871673a2e93105ecc5d774
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-02-13 21:50:27 +0100

    solucion final

--- alcocer/haproxy-php/hamgr.sh
+++ alcocer/haproxy-php/hamgr.sh
@@ -2,9 +2,4 @@
 
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.conf -Ds
 
-CHOICE='foo'
-while [[ ${CHOICE,,} != 'salir' ]]; do
-        printf "Escribe 'salir' para terminar el contenedor, o bien, 'docker stop <contenedor>'\n"
-        read -p 'haproxy hosting cmd: ' CHOICE
-done
 /usr/bin/killall haproxy
--- david/apache/presta.sh
+++ david/apache/presta.sh
@@ -6,4 +6,5 @@ if [[ ! -d /srv/www/prestashop ]]; then
     wget -P /tmp  https://download.prestashop.com/download/releases/prestashop_1.6.1.11.zip
     unzip -d /srv/www /tmp/prestashop_1.6.1.11.zip
     chown www-data. -R /srv
+    sed -i "s/define('_PS_MODE_DEV_', false)/define('_PS_MODE_DEV_', true)/" /srv/www/prestashop/config/defines.inc.php
 fi
